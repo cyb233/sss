20af4640c4b78d972cfbbe19a4e27d6dc42fc91b
qianjianyu <1934492613@qq.com>
2022-02-03 14:59:18 +0800

    update GoodsDao and Add , JpaSpecificationExecutor<Order>

--- src/main/java/moe/shuvi/dao/GoodsDao.java
+++ src/main/java/moe/shuvi/dao/GoodsDao.java
@@ -1,10 +1,12 @@
 package moe.shuvi.dao;
 
 import moe.shuvi.model.Goods;
+import moe.shuvi.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 /**
  * @author qjy
  */
-public interface GoodsDao extends JpaRepository<Goods,Integer> {
+public interface GoodsDao extends JpaRepository<Goods,Integer>, JpaSpecificationExecutor<Order>  {
 }
