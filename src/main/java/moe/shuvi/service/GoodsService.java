11205260428af1860e2233882edaca6e2e0e5b5a
jianyu qian <68489513+qianjianyu7566@users.noreply.github.com>
2022-02-04 12:07:45 +0800

    添加了商品的增删改查

--- src/main/java/moe/shuvi/service/GoodsService.java
+++ src/main/java/moe/shuvi/service/GoodsService.java
@@ -1,4 +1,13 @@
 package moe.shuvi.service;
 
+import moe.shuvi.model.Goods;
+import moe.shuvi.utils.Result;
+
 public interface GoodsService {
+
+    Result findByPage(Goods goods,int pageNow,int pageSize) throws Exception;
+
+    Result addGoods(Goods goods) throws Exception;
+
+    Result removeGoodsById(int id) throws Exception;
 }
