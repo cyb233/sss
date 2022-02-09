f167728643d1a104a3654a3d462fccc042313562
Schwi <39186981+cyb233@users.noreply.github.com>
2022-02-09 15:24:42 +0800

    Update UserController.java

--- src/main/java/moe/shuvi/controller/UserController.java
+++ src/main/java/moe/shuvi/controller/UserController.java
@@ -21,7 +21,7 @@ public class UserController {
     private UserService userService;
     @ApiImplicitParams({
             @ApiImplicitParam(name = "username", value = "用户名", required = true ,dataType = "string"),
-            @ApiImplicitParam(name = "passwd", value = "密码", required = true ,dataType = "string")
+            @ApiImplicitParam(name = "password", value = "密码", required = true ,dataType = "string")
     })
     @LogAnnotation(title = "用户模块",action = "用户登录")
     @RequestMapping("/login")
